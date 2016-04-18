10626944fc252bcdda60372e69b159000ce83577
Frans van Basten - Digitect <Frans.van.Basten@digitect.nl>
2016-04-18 17:03:43 +0100

    Backup repository if it has more revisions than last time it was backed up.

--- svnbackup.sh
+++ svnbackup.sh
@@ -3,45 +3,45 @@
 # A script to make a backup of all MySQL databases.
 
 usage() {
-  echo "Usage: $0 -u username -p password"
+  echo "Usage: $0 [-p PATH] [-r REPOSITORY] [-b BACKUPLOCATION] [-c]"
   echo
+  echo "  -p PATH"
+  echo "    The path where the repositories are located."
+  echo "    Default: /repos"
   echo "  -r REPOSITORY"
-  echo "    The repository to backup, this is a path to the repository."
+  echo "    The name of the repository to backup (without the path)."
+  echo "    Default: all repositories in PATH"
   echo "  -b BACKUPLOCATION"
   echo "    The location to store the backups to."
-  echo "    Default: /tmp."
-  echo "  -z"
+  echo "    Default: /tmp"
+  echo "  -c"
   echo "    Indicated compression (.zip) should be used."
-  echo "    Default: not set."
+  echo "    Default: not compressed."
   exit 1
 }
 
 readargs() {
   while [ "$#" -gt 0 ] ; do
     case "$1" in
+      -p)
+	if [ "$2" ] ; then
+	  path="$2"
+	  shift ; shift
+	fi
+      ;;
       -r)
         if [ "$2" ] ; then
           repository="$2"
           shift ; shift
-        else
-          echo "Missing a value for $1."
-          echo
-          shift
-          usage
         fi
       ;;
       -b)
         if [ "$2" ] ; then
           backuplocation="$2"
           shift ; shift
-        else
-          echo "Missing a value for $1."
-          echo
-          shift
-          usage
         fi
       ;;
-      -z)
+      -c)
         compression="yes"
         shift
       ;;
@@ -55,15 +55,21 @@ readargs() {
   done
 }
 
-checkargs() {
-  if [ ! "${repository}" ] ; then
-    echo "Missing repository."
-    echo
-    usage
-  fi
-}
+#checkargs() {
+#  if [ ! "${repository}" ] ; then
+#    echo "Missing repository."
+#    echo
+#    usage
+#  fi
+#}
 
 setargs() {
+  if [ ! "${path}" ] ; then
+    path="/repos"
+  fi
+  if [ ! "${repository}" ] ; then
+    repository="*"
+  fi
   if [ ! "${backuplocation}" ] ; then
     backuplocation="/tmp"
   fi
@@ -72,21 +78,44 @@ setargs() {
 checkvalues() {
   if [ ! -d "${backuplocation}" ] ; then
     echo "${backuplocation} is not a directory."
-    echo
     usage
+  fi
+  if [ "$repository" != "*" ] ; then 
+    if [ ! -d "${path}/${repository}" ] ; then       
+      echo "${path}${repository} is not a valid path with repository"
+      usage
+    fi
   fi
 }
 
 main() {
-  if [ "${compression}" ] ; then
-    svnadmin dump ${repository} | zip ${backuplocation}/$(basename ${repository}).dump.zip -
-  else
-    svnadmin dump ${repository} > ${backuplocation}/$(basename ${repository}).dump
-  fi
+  
+  for repos in $path/$repository ; do
+    echo $repos
+    base=$(basename ${repos})
+    revcurr=$(svnadmin verify /repos/test 2>&1 | tail -1 | sed -e 's/[^0-9]//g')
+    if [ -f ${backuplocation}/${base}.recent ] ; then
+      revlast=$(cat ${backuplocation}/${base}.recent)
+       if [ "${revlast}" == "${revcurr}" ]; then
+        echo "No changes for repository ${repos}."
+      else
+        echo "Repository ${repos} updated from revision ${revlast} to ${revcurr}"
+        if [ "${compression}" ] ; then
+          svnadmin dump ${repos} | gzip -9 > ${backuplocation}/${base}.dump.gz
+        else
+          svnadmin dump ${repos} > ${backuplocation}/${base}.dump
+        fi
+        echo "${revcurr}" > ${backuplocation}/${base}.recent
+      fi
+    else
+      echo "No recent file found for ${repos}, last backed up revision set to 0."
+      echo "0" > ${backuplocation}/${base}.recent
+    fi
+  done
 }
 
 readargs "$@"
-checkargs
+#checkargs
 setargs
 checkvalues
 main
