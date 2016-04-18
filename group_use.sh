8f45ed5f38403171e50768877fde0fd9cb36045f
Frans van Basten - Digitect <Frans.van.Basten@digitect.nl>
2016-04-18 13:07:55 +0100

    Specific parameters replaced with generic parameters.

--- group_use.sh
+++ group_use.sh
@@ -1,9 +1,9 @@
 # Script to set group use for a directory
-USER=tools
-GROUP=tools
-DIR=/private
+# Replace <linux-user>, <linux-group> and <directory> and run.
+USER=<linux-user>
+GROUP=<linux-group>
+DIR=<directory>
 chown -R $USER:$GROUP $DIR
 find $DIR -exec chmod g+u {} \;
 find $DIR -exec chmod o-rwx {} \;
 find $DIR -type d -exec chmod g+s {} \;
-# svnadmin load /repos/SalarisRecover < /home/dump/Salaris.dump
